e0c832ea260701dfccc08837e6b3109103ef9822
Sieun Park <87406514+sieunp06@users.noreply.github.com>
2024-04-23 13:40:52 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.js
+++ JavaScript/프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.js
@@ -1,9 +1,7 @@
 function solution(n) {
     var answer = [];
-    for (var i = 1; i <= n; i++) {
-        if (i % 2 != 0) {
-            answer.push(i);
-        }
+    for (let i = 1; i <= n; i += 2) {
+        answer.push(i);
     }
     return answer;
 }
