fe2e9a53a701f54923e1d9f41f972cf61e9fd9be
Sieun Park <87406514+sieunp06@users.noreply.github.com>
2024-06-10 16:41:47 +0900

    [level 0] Title: 접미사 배열, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/0/181909. 접미사 배열/접미사 배열.js
@@ -0,0 +1,9 @@
+function solution(my_string) {
+    var answer = [];
+    
+    for (let i = 0; i < my_string.length; i++) {
+        answer.push(my_string.substring(i, my_string.length));
+    }
+    
+    return answer.sort();
+}
