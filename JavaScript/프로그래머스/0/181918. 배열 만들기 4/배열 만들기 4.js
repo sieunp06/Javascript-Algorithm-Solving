68db9d87700c1b3813b373494b7ed8439ccf6c5b
Sieun Park <87406514+sieunp06@users.noreply.github.com>
2024-06-13 11:41:10 +0900

    [level 0] Title: 배열 만들기 4, Time: 2.84 ms, Memory: 37 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.js
@@ -0,0 +1,23 @@
+function solution(arr) {
+    var stk = [];
+    
+    var i = 0;
+    
+    while (i < arr.length) {
+        if (stk.length === 0) {
+            stk.push(arr[i]);
+            i++;
+            continue;
+        } 
+        if (stk[stk.length - 1] < arr[i]) {
+            stk.push(arr[i]);
+            i++;
+            continue;
+        }
+        if (stk[stk.length - 1] >= arr[i]) {
+            stk.pop();
+        }
+    }
+    
+    return stk;
+}
