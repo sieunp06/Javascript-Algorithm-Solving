05eff27ba7cf775943aebb69e4e6b46e854f296c
Sieun Park <87406514+sieunp06@users.noreply.github.com>
2024-07-04 14:43:04 +0900

    [level 1] Title: 약수의 합, Time: 0.45 ms, Memory: 33.6 MB -BaekjoonHub

--- JavaScript/프로그래머스/1/12928. 약수의 합/약수의 합.js
+++ JavaScript/프로그래머스/1/12928. 약수의 합/약수의 합.js
@@ -1,11 +1,22 @@
-function solution(n) {
-    var answer = 0;
-    
-    for (let i = 1; i <= n; i++) {
-        if (n % i == 0) {
-            answer += i;
-        }
+const solution = (s) => {
+    return makeDivisorList(s).reduce((pre, cur) => (pre + cur), 0);
+}
+
+const makeDivisorList = (targetNum) => {
+    return makeRangeList(targetNum).filter(num => isDivisor(targetNum, num));
+}
+
+const makeRangeList = (targetNum) => {
+    const rangeNums = [];
+    for (let i = 1; i <= targetNum; i++) {
+        rangeNums.push(i);
     }
-    
-    return answer;
+    return rangeNums;
+}
+
+const isDivisor = (targetNum, divisorNum) => {
+    if (targetNum % divisorNum === 0) {
+        return true;
+    }
+    return false;
 }
